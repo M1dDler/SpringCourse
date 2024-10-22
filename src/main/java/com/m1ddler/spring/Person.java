f12a2398b69404afda918d6f64d9877608a7d625
M1dDler <m1ddler.ua@gmail.com>
2024-10-22 16:20:59 +0300

    Set other Person fields. Using setter DI

--- src/main/java/com/m1ddler/spring/Person.java
+++ src/main/java/com/m1ddler/spring/Person.java
@@ -2,10 +2,39 @@ package com.m1ddler.spring;
 
 public class Person {
     private Pet pet;
+    private String SurName;
+    private int age;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person set pet");
         this.pet = pet;
+    }
+
+    public String getSurName() {
+        return SurName;
+    }
+
+    public void setSurName(String surName) {
+        System.out.println("Class Person: set surName");
+        this.SurName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet(){
