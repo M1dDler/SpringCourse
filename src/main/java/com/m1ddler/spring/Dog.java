20880d31688c05ed22f31c600b1376f03895752f
M1dDler <m1ddler.ua@gmail.com>
2024-10-24 15:05:57 +0300

    Singleton and prototype scopes

--- src/main/java/com/m1ddler/spring/Dog.java
+++ src/main/java/com/m1ddler/spring/Dog.java
@@ -1,8 +1,18 @@
 package com.m1ddler.spring;
 
 public class Dog implements Pet{
+    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
